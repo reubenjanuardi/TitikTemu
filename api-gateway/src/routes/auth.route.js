4ee63770e857725f7fc2f071d8b9d8ccad5d2f8a
reubenjanuardi <reubenjanuardi@gmail.com>
2025-12-17 22:42:10 +0700

    fix: add pathRewrite configuration to authProxy for correct routing

--- api-gateway/src/routes/auth.route.js
+++ api-gateway/src/routes/auth.route.js
@@ -7,6 +7,9 @@ const router = express.Router();
 const authProxy = createProxyMiddleware({
   target: process.env.AUTH_SERVICE_URL,
   changeOrigin: true,
+  pathRewrite: {
+    '^/api/auth': '/auth'
+  },
 });
 
 // Public routes
